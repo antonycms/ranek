3d2cf1edd734b979bdc36ef32ec7fd46f80074b6
Antony <antonyc828@gmail.com>
2020-07-22 12:21:07 -0300

    feat: finished Header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,65 @@
 <template>
   <div id="app">
-    <div><router-link to="/">Home</router-link> |</div>
+    <Header />
+
     <router-view />
+
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
 export default {
   name: 'App',
+  components: {
+    Header,
+    Footer,
+  },
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+ul {
+  list-style: none;
+}
+
+html,
+body,
+#app {
+  width: 100%;
+  height: 100vh;
+}
+
+.btn {
+  background: #87f;
+  color: white;
+  box-shadow: 0px 2px 8px rgba(30, 60, 90, 0.2);
+
+  border: 0;
+  border-radius: 4px;
+  padding: 10px 30px;
+
+  font-size: 1rem;
+  text-align: center;
+
+  cursor: pointer;
+
+  transition: all 0.3s;
+}
+
+.btn:hover {
+  transform: scale(1.1);
+  background: #65d;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav class="container">
+      <router-link class="logo" :to="{ name: 'Home' }">
+        <img src="@/assets/ranek.svg" alt="Ranek" />
+      </router-link>
+
+      <router-link tag="button" class="btn" to="/">
+        Vender / Login
+      </router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 20px 30px;
+
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+
+.logo img {
+  width: 90px;
+}
+</style>
